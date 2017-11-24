e7f71cf43a75bcb38f7ce186c93ed2a247a5f45a
serendipityapps <info@serendipityappcompany.com>
2017-11-24 23:55:41 +0000

    adjustments

--- Source/Extensions/ConstraintsSetup.swift
+++ Source/Extensions/ConstraintsSetup.swift
@@ -42,7 +42,7 @@ extension BottomContainerView {
 		
 		addConstraint(NSLayoutConstraint(item: actionButton, attribute: .width,
 																		 relatedBy: .greaterThanOrEqual, toItem: nil, attribute: .notAnAttribute,
-																		 multiplier: 1, constant: 100))
+																		 multiplier: 1, constant: 86))
 
     addConstraint(NSLayoutConstraint(item: stackView, attribute: .centerY,
       relatedBy: .equal, toItem: self, attribute: .centerY,
@@ -69,21 +69,21 @@ extension BottomContainerView {
 extension TopView {
 
   func setupConstraints() {
-    addConstraint(NSLayoutConstraint(item: flashButton, attribute: .left,
-      relatedBy: .equal, toItem: self, attribute: .left,
+    addConstraint(NSLayoutConstraint(item: flashButton, attribute: .leading,
+      relatedBy: .equal, toItem: self, attribute: .leading,
       multiplier: 1, constant: Dimensions.leftOffset))
 
     addConstraint(NSLayoutConstraint(item: flashButton, attribute: .centerY,
       relatedBy: .equal, toItem: self, attribute: .centerY,
       multiplier: 1, constant: 0))
 
-//    addConstraint(NSLayoutConstraint(item: flashButton, attribute: .width,
-//      relatedBy: .equal, toItem: nil, attribute: .notAnAttribute,
-//      multiplier: 1, constant: 55))
+    addConstraint(NSLayoutConstraint(item: flashButton, attribute: .width,
+      relatedBy: .greaterThanOrEqual, toItem: nil, attribute: .notAnAttribute,
+      multiplier: 1, constant: 86))
 
     if configuration.canRotateCamera {
-      addConstraint(NSLayoutConstraint(item: rotateCamera, attribute: .right,
-        relatedBy: .equal, toItem: self, attribute: .right,
+      addConstraint(NSLayoutConstraint(item: rotateCamera, attribute: .trailing,
+        relatedBy: .equal, toItem: self, attribute: .trailing,
         multiplier: 1, constant: Dimensions.rightOffset))
 
       addConstraint(NSLayoutConstraint(item: rotateCamera, attribute: .centerY,
