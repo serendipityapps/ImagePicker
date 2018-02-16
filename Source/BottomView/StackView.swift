ddcff374b6ee1ca1a62b4d9f5aec23a52b5c31a2
serendipityapps <info@serendipityappcompany.com>
2018-02-16 16:10:25 +0000

    fix

--- Source/BottomView/StackView.swift
+++ Source/BottomView/StackView.swift
@@ -52,6 +52,7 @@ class ImageStackView: UIView {
 
     views.forEach { addSubview($0) }
 
+		imageCountBadge.translatesAutoresizingMaskIntoConstraints = false
 		addSubview(imageCountBadge)
 		addConstraint(NSLayoutConstraint(item: imageCountBadge, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1.0, constant: 0))
 		addConstraint(NSLayoutConstraint(item: imageCountBadge, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: self.configuration.stackViewImageSize.height))
